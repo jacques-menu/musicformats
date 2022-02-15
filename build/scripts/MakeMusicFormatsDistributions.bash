76e2e980684fc0ec1b543ab30e9e890a55966e07
jacques-menu <imj-muzhic@bluewin.ch>
2022-02-15 17:37:53 +0100

    Distrib test 16

--- build/scripts/MakeMusicFormatsDistributions.bash
+++ build/scripts/MakeMusicFormatsDistributions.bash
@@ -17,9 +17,9 @@ MUSIC_FORMATS_DEV=${HOME}/musicformats-git-dev
 
 DISTRIB_DIR=${MUSIC_FORMATS_DEV}/distrib
 
-DOWNLOADED_MACOS_DISTRIB_NAME="/musicformats-macos-distrib"
-DOWNLOADED_UBUNTU_DISTRIB_NAME="/musicformats-ubuntu-distrib"
-DOWNLOADED_WINDOWS_DISTRIB_NAME="/musicformats-windows-distrib"
+DOWNLOADED_MACOS_DISTRIB_NAME="musicformats-macos-distrib"
+DOWNLOADED_UBUNTU_DISTRIB_NAME="musicformats-ubuntu-distrib"
+DOWNLOADED_WINDOWS_DISTRIB_NAME="musicformats-windows-distrib"
 
 MACOS_DISTRIB_ORG="${DISTRIB_DIR}/musicformats-macos-distrib"
 UBUNTU_DISTRIB_ORG="${DISTRIB_DIR}/musicformats-ubuntu-distrib"
@@ -35,20 +35,20 @@ function MakeDocumentationDistribution ()
   echo "----------------------------------------------"
   echo
 
+  cd ${DISTRIB_DIR}
   echo -n "--> current directory: "; pwd
   echo
 
-   # copy documentation files from MACOS_DISTRIB_ORG to DISTRIB_DIR
+  echo "--> copy documentation files from ${MACOS_DISTRIB_ORG} to ${DISTRIB_DIR}"
   cp -p ${MACOS_DISTRIB_ORG}/MusicFormatsVersionNumber.txt ${DISTRIB_DIR}
 
   cp -p ${MACOS_DISTRIB_ORG}/documentation/*/*.pdf ${DISTRIB_DIR}
 
-  # create the Mac OS documentation zip archive
-#  zip MAC_OS_ZIP -r ${MAC_OS_DMG}
-
   echo "Mac OS documentation distribution:"
   ls -sal ${DISTRIB_DIR}
+  echo
 }
+
 
 function MakeMacOSDistribution ()
 {
@@ -57,45 +57,57 @@ function MakeMacOSDistribution ()
   echo "----------------------------------------------"
   echo
 
-  echo "--> MACOS_DISTRIB_ORG   = ${MACOS_DISTRIB_ORG}"
-  echo
-
-  MAC_OS_DMG_NAME="MusicFormatsForMacOS"
-
-  MAC_OS_DMG="${DISTRIB_DIR}/${MAC_OS_DMG_NAME}.dmg"
-  echo "--> MAC_OS_DMG   = ${MAC_OS_DMG}"
-  echo
-
-  MAC_OS_ZIP="${DISTRIB_DIR}/MusicFormatsForMacOS.zip"
-  echo "--> MAC_OS_DMG   = ${MAC_OS_DMG}"
-  echo
-
+  cd ${DISTRIB_DIR}
   echo -n "--> current directory: "; pwd
   echo
 
-  MACOS_DISTRIB_WORK="${DISTRIB_DIR}/MusicFormatsForMacOS"
+  echo "--> MACOS_DISTRIB_ORG   = ${MACOS_DISTRIB_ORG}"
+  echo
+
+#  MAC_OS_DMG_NAME="MusicFormatsForMacOS"
+
+#  MAC_OS_DMG="${DISTRIB_DIR}/${MAC_OS_DMG_NAME}.dmg"
+#  echo "--> MAC_OS_DMG   = ${MAC_OS_DMG}"
+#  echo
+
+  MAC_OS_ZIP_NAME="MusicFormatsForMacOS.zip"
+  echo "--> MAC_OS_ZIP_NAME   = ${MAC_OS_ZIP_NAME}"
+  echo
+
+  MAC_OS_ZIP="${DISTRIB_DIR}/${MAC_OS_ZIP_NAME}"
+  echo "--> MAC_OS_ZIP_NAME   = ${MAC_OS_ZIP_NAME}"
+  echo
+
+  MACOS_DISTRIB_WORK_NAME="MusicFormatsForMacOS"
+  echo "--> MACOS_DISTRIB_WORK_NAME   = ${MACOS_DISTRIB_WORK_NAME}"
+  echo
+
+  MACOS_DISTRIB_WORK="${DISTRIB_DIR}/${MACOS_DISTRIB_WORK_NAME}"
   echo "--> MACOS_DISTRIB_WORK   = ${MACOS_DISTRIB_WORK}"
   echo
 
-  [ -e ${MACOS_DISTRIB_WORK} ]; rm -r ${MACOS_DISTRIB_WORK}
+set -x
+  echo "--> make sure ${MACOS_DISTRIB_WORK_NAME} is empty"
+  [ -e ${MACOS_DISTRIB_WORK_NAME} ]; rm -r ${MACOS_DISTRIB_WORK_NAME}
+set +x
 
-  mkdir ${MACOS_DISTRIB_WORK}
+  mkdir ${MACOS_DISTRIB_WORK_NAME}
+  echo
 
   echo "--> ${MACOS_DISTRIB_WORK} initial contents:"
   ls -sal ${MACOS_DISTRIB_WORK}
   echo
 
+  echo "--> cd ${MACOS_DISTRIB_WORK}"
   cd ${MACOS_DISTRIB_WORK}
   echo -n "--> current directory: "; pwd
   echo
 
-  # copy files from MACOS_DISTRIB_ORG to MACOS_DISTRIB_WORK
+  echo "--> copy files from ${MACOS_DISTRIB_ORG_NAME} to ${MACOS_DISTRIB_WORK_NAME}"
   cp -p ${MACOS_DISTRIB_ORG}/MusicFormatsVersionNumber.txt ${MACOS_DISTRIB_WORK}
 
   cp -pr ${MACOS_DISTRIB_ORG}/build/bin ${MACOS_DISTRIB_WORK}
-
-#  mkdir ${MACOS_DISTRIB_WORK}/documentation
-#  cp -p ${MACOS_DISTRIB_ORG}/documentation/*/*.pdf ${MACOS_DISTRIB_WORK}/documentation
+  echo
 
   echo "--> after files copy:"
   echo
@@ -115,24 +127,26 @@ function MakeMacOSDistribution ()
   bin/xml2ly -contact
   echo
 
-  # create the Mac OS DMG image
+  echo "--> create the Mac OS zip archive"
   cd ${DISTRIB_DIR}
   echo -n "--> current directory: "; pwd
   echo
 
-  [ -e ${MAC_OS_DMG} ]; rm -r ${MAC_OS_DMG}
-
-  hdiutil create ${MAC_OS_DMG} -ov -volname ${MAC_OS_DMG_NAME} -fs APFS -srcfolder ${MACOS_DISTRIB_WORK}
+  zip ${MAC_OS_ZIP_NAME} -r ${MACOS_DISTRIB_WORK_NAME}
   echo
 
-  echo "Mac OS distribution DMG:"
-  ls -sal ${MAC_OS_DMG}
+  echo "Mac OS distribution zip:"
+  ls -sal ${MAC_OS_ZIP}
+  echo
 
-  # create the Mac OS zip archive
-#  zip MAC_OS_ZIP -r ${MAC_OS_DMG}
+#  echo "--> create the Mac OS DMG"
+#  [ -e ${MAC_OS_DMG} ]; rm -r ${MAC_OS_DMG}
 
-#  echo "Mac OS distribution zip:"
-#  ls -sal ${MAC_OS_ZIP}
+#  hdiutil create ${MAC_OS_DMG} -ov -volname ${MAC_OS_DMG_NAME} -fs APFS -srcfolder ${MAC_OS_ZIP}
+#  echo
+#
+#  echo "Mac OS distribution DMG:"
+#  ls -sal ${MAC_OS_DMG}
 }
 
 
@@ -143,55 +157,67 @@ function MakeUbuntuDistribution ()
   echo "----------------------------------------------"
   echo
 
-  echo "--> UBUNTU_DISTRIB_ORG   = ${UBUNTU_DISTRIB_ORG}"
-  echo
-
-  UBUNTU_ZIP="${DISTRIB_DIR}/MusicFormatsForUbuntu.zip"
-  echo "--> UBUNTU_ZIP   = ${UBUNTU_ZIP}"
-  echo
-
+  cd ${DISTRIB_DIR}
   echo -n "--> current directory: "; pwd
   echo
 
-  UBUNTU_DISTRIB_WORK="${DISTRIB_DIR}/MusicFormatsForUbuntu"
+  echo "--> UBUNTU_DISTRIB_ORG   = ${UBUNTU_DISTRIB_ORG}"
+  echo
+
+  UBUNTU_ZIP_NAME="MusicFormatsForUbuntu.zip"
+  echo "--> UBUNTU_ZIP_NAME   = ${UBUNTU_ZIP_NAME}"
+  echo
+
+  UBUNTU_ZIP="${DISTRIB_DIR}/${DISTRIB_DIR}"
+  echo "--> UBUNTU_ZIP   = ${UBUNTU_ZIP_NAME}"
+  echo
+
+  UBUNTU_DISTRIB_WORK_NAME="MusicFormatsForUbuntu"
+  echo "--> UBUNTU_DISTRIB_WORK_NAME   = ${UBUNTU_DISTRIB_WORK_NAME}"
+  echo
+
+  UBUNTU_DISTRIB_WORK="${DISTRIB_DIR}/${UBUNTU_DISTRIB_WORK_NAME}"
   echo "--> UBUNTU_DISTRIB_WORK   = ${UBUNTU_DISTRIB_WORK}"
   echo
 
-  [ -e ${UBUNTU_DISTRIB_WORK} ]; rm -r ${UBUNTU_DISTRIB_WORK}
+  echo "--> make sure ${UBUNTU_DISTRIB_WORK_NAME} is empty"
+  [ -e ${UBUNTU_DISTRIB_WORK_NAME} ]; rm -r ${UBUNTU_DISTRIB_WORK_NAME}
 
-  mkdir ${UBUNTU_DISTRIB_WORK}
-
-  echo "--> ${UBUNTU_DISTRIB_WORK} initial contents:"
-  ls -sal ${UBUNTU_DISTRIB_WORK}
+  mkdir ${UBUNTU_DISTRIB_WORK_NAME}
   echo
 
+  echo "--> ${UBUNTU_DISTRIB_WORK_NAME} initial contents:"
+  ls -sal ${UBUNTU_DISTRIB_WORK_NAME}
+  echo
+
+  echo "--> cd ${UBUNTU_DISTRIB_WORK}"
   cd ${UBUNTU_DISTRIB_WORK}
   echo -n "--> current directory: "; pwd
   echo
 
-  # copy files from UBUNTU_DISTRIB_ORG to UBUNTU_DISTRIB_WORK
+  echo "--> copy files from ${UBUNTU_DISTRIB_ORG_NAME} to ${UBUNTU_DISTRIB_WORK_NAME}"
   cp -p ${UBUNTU_DISTRIB_ORG}/MusicFormatsVersionNumber.txt ${UBUNTU_DISTRIB_WORK}
 
   cp -pr ${UBUNTU_DISTRIB_ORG}/build/bin ${UBUNTU_DISTRIB_WORK}
   cp -pr ${UBUNTU_DISTRIB_ORG}/build/lib ${UBUNTU_DISTRIB_WORK}
-
-#  mkdir ${UBUNTU_DISTRIB_WORK}/documentation
-#  cp -p ${UBUNTU_DISTRIB_ORG}/documentation/*/*.pdf ${UBUNTU_DISTRIB_WORK}/documentation
+  echo
 
   echo "--> after files copy:"
   echo
   ls -salR
   echo
 
-  # create the Ubuntu zip archive
+  echo "--> create the Ubuntu zip archive"
   cd ${DISTRIB_DIR}
   echo -n "--> current directory: "; pwd
   echo
 
-  zip ${UBUNTU_ZIP} -r ${UBUNTU_DISTRIB_WORK}
+  zip ${UBUNTU_ZIP_NAME} -r ${UBUNTU_DISTRIB_WORK_NAME}
+  echo
 
   echo "Ubuntu distribution:"
   ls -sal ${UBUNTU_ZIP}
+  echo
 }
 
 
@@ -202,68 +228,81 @@ function MakeWindowsDistribution ()
   echo "----------------------------------------------"
   echo
 
-  echo "--> WINDOWS_DISTRIB_ORG   = ${WINDOWS_DISTRIB_ORG}"
-  echo
-
-  WINDOWS_ZIP="${DISTRIB_DIR}/MusicFormatsForWindows.zip"
-  echo "--> WINDOWS_ZIP   = ${WINDOWS_ZIP}"
-  echo
-
+  cd ${DISTRIB_DIR}
   echo -n "--> current directory: "; pwd
   echo
 
-  WINDOWS_DISTRIB_WORK="${DISTRIB_DIR}/MusicFormatsForWindows"
+  echo "--> WINDOWS_DISTRIB_ORG   = ${WINDOWS_DISTRIB_ORG}"
+  echo
+
+  WINDOWS_ZIP_NAME="MusicFormatsForWindows.zip"
+  echo "--> WINDOWS_ZIP_NAME   = ${WINDOWS_ZIP_NAME}"
+  echo
+
+  WINDOWS_ZIP="${DISTRIB_DIR}/${WINDOWS_ZIP_NAME}"
+  echo "--> WINDOWS_ZIP   = ${WINDOWS_ZIP}"
+  echo
+
+  WINDOWS_DISTRIB_WORK_NAME="MusicFormatsForWindows"
+  echo "--> WINDOWS_DISTRIB_WORK_NAME   = ${WINDOWS_DISTRIB_WORK_NAME}"
+  echo
+
+  WINDOWS_DISTRIB_WORK="${DISTRIB_DIR}/${WINDOWS_DISTRIB_WORK_NAME}"
   echo "--> WINDOWS_DISTRIB_WORK   = ${WINDOWS_DISTRIB_WORK}"
   echo
 
-  [ -e ${WINDOWS_DISTRIB_WORK} ]; rm -r ${WINDOWS_DISTRIB_WORK}
+  echo "--> make sure $WINDOWS_DISTRIB_WORK_NAME} is empty"
+  [ -e ${WINDOWS_DISTRIB_WORK_NAME} ]; rm -r ${WINDOWS_DISTRIB_WORK_NAME}
 
-  mkdir ${WINDOWS_DISTRIB_WORK}
-
-  echo "--> ${WINDOWS_DISTRIB_WORK} initial contents:"
-  ls -sal ${WINDOWS_DISTRIB_WORK}
+  mkdir ${WINDOWS_DISTRIB_WORK_NAME}
   echo
 
+  echo "--> ${WINDOWS_DISTRIB_WORK_NAME} initial contents:"
+  ls -sal ${WINDOWS_DISTRIB_WORK_NAME}
+  echo
+
+  echo "--> cd ${WINDOWS_DISTRIB_WORK}"
   cd ${WINDOWS_DISTRIB_WORK}
   echo -n "--> current directory: "; pwd
   echo
 
-  # copy files from WINDOWS_DISTRIB_ORG to WINDOWS_DISTRIB_WORK
+  echo "--> copy files from ${WINDOWS_DISTRIB_ORG_NAME} to ${WINDOWS_DISTRIB_WORK_NAME}"
   cp -p ${WINDOWS_DISTRIB_ORG}/MusicFormatsVersionNumber.txt ${WINDOWS_DISTRIB_WORK}
 
   cp -pr ${WINDOWS_DISTRIB_ORG}/build/bin ${WINDOWS_DISTRIB_WORK}
   cp -pr ${WINDOWS_DISTRIB_ORG}/build/lib ${WINDOWS_DISTRIB_WORK}
-
-#  mkdir ${WINDOWS_DISTRIB_WORK}/documentation
-#  cp -p ${WINDOWS_DISTRIB_ORG}/documentation/*/*.pdf ${WINDOWS_DISTRIB_WORK}/documentation
+  echo
 
   echo "--> after files copy:"
   echo
   ls -salR
   echo
 
-  # create the Windows zip archive
+  echo "--> create the Windows zip archive"
   cd ${DISTRIB_DIR}
   echo -n "--> current directory: "; pwd
   echo
 
-  zip ${WINDOWS_ZIP} -r ${WINDOWS_DISTRIB_WORK}
+  zip ${WINDOWS_ZIP_NAME} -r ${WINDOWS_DISTRIB_WORK_NAME}
+  echo
 
   echo "Windows distribution:"
   ls -sal ${WINDOWS_ZIP}
+  echo
 }
 
 
-# Let's go!
-# -----------------------------------------
+echo "----------------------------------------------"
+echo "==> Let's go!"
+echo "----------------------------------------------"
+echo
 
 cd ${DISTRIB_DIR}
 echo -n "--> current directory: "; pwd
 echo
 
-
 echo "----------------------------------------------"
-echo "downloaded distributions:"
+echo "==> downloaded distributions:"
 echo "----------------------------------------------"
 echo
 
@@ -273,19 +312,20 @@ ls -sal ${DOWNLOADED_DISTRIBUTIONS}
 echo
 
 echo "----------------------------------------------"
-echo "distrib directory:"
+echo "==> distrib directory:"
 echo "----------------------------------------------"
 echo
 
 echo "--> DISTRIB_DIR   = ${DISTRIB_DIR}"
 echo
 
-echo "--> ${DISTRIB_DIR} initial contents:"
+echo "--> initial ${DISTRIB_DIR} contents:"
 ls -sal ${DISTRIB_DIR}
 echo
 
-# copy the distributions to the MusicFormats distrib folder
+echo "--> copy the distributions to the MusicFormats distrib folder"
 cp -pr ${DOWNLOADED_DISTRIBUTIONS} ${DISTRIB_DIR}
+echo
 
 echo "--> ${DISTRIB_DIR} contents after copy:"
 ls -sal ${DISTRIB_DIR}
@@ -293,7 +333,7 @@ echo; echo
 
 
 echo "----------------------------------------------"
-echo "make the distributions:"
+echo "==> make the distributions:"
 echo "----------------------------------------------"
 echo
 
@@ -308,8 +348,9 @@ echo
 
 MakeWindowsDistribution
 
+
 echo "----------------------------------------------"
-echo "distrib contents:"
+echo "==> final distrib contents:"
 echo "----------------------------------------------"
 echo
 
