442bb58b6af0a7a8e0863b809ad2b396d45ec247
Jacques Menu <imj-219@bluewin.ch>
2025-06-07 15:13:32 +0200

    v0.9.75-dev - 2025_06_07

--- src/mfutilities/mfStack.h
+++ src/mfutilities/mfStack.h
@@ -24,114 +24,114 @@ namespace MusicFormats
 {
 
 //______________________________________________________________________________
-template <typename ELEMENT_TYPE>
-class EXP mfStack : public smartable
-/*
-  C++ std::stack cannot be traversed, so we use std::list instead
-
-  https://www.programiz.com/cpp-programming/typename-templates
-
-  https://en.cppreference.com/w/cpp/language/type_alias
-
-  Do not put templated functions into the cpp file.
-  Since they are not implementations they are not found by the compiler/linker.
-  So leave them in the header file.
-
-  member reference base type 'int' is not a structure or union
-
-  https://stackoverflow.com/questions/580922/identifying-primitive-types-in-templates
-
-  https://en.cppreference.com/w/cpp/language/template_specialization
-
-  https://www.ibm.com/docs/en/zos/2.4.0?topic=only-explicit-specialization-c
-
-  https://dev.to/sandordargo/what-are-type-traits-in-c-18j5
-*/
-{
-  public:
-
-    // creation
-    // ------------------------------------------------------
-
-    static SMARTP<mfStack> create ();
-
-    static SMARTP<mfStack> create (
-                            const ELEMENT_TYPE& element);
-
-    // constructors/destructor
-    // ------------------------------------------------------
-
-                          mfStack ();
-
-                          mfStack (const ELEMENT_TYPE& element);
-
-    virtual               ~mfStack ();
-
-  public:
-
-    // set and get
-    // ------------------------------------------------------
-
-    const std::list <ELEMENT_TYPE>&
-                          getElementsStdList () const
-                              { return fElementsStdList;}
-
-  public:
-
-    // public services
-    // ------------------------------------------------------
-
-    // basic list stuff
-    size_t                size () const
-                              { return fElementsStdList.size (); }
-
-    void                  push (
-                            const ELEMENT_TYPE& element)
-                              { fElementsStdList.push_front (element); }
-
-    ELEMENT_TYPE&         top ()
-                              { return fElementsStdList.front (); }
-
-    void                  pop ()
-                              { fElementsStdList.pop_front (); }
-
-    // specific stack stuff
-    Bool                  contains (
-                            const ELEMENT_TYPE& value) const;
-
-  public:
-
-    // print
-    // ------------------------------------------------------
-
-    void                  print (std::ostream& os) const;
-
-    void                  printWithContext (
-                            const std::string& context,
-                            char               evidencer,
-                            std::ostream&      os) const;
-
-  private:
-
-    // private services
-    // ------------------------------------------------------
-
-  private:
-
-    // private fields
-    // ------------------------------------------------------
-
-    std::string           fElementsStackName;
-
-    std::list <ELEMENT_TYPE>
-                          fElementsStdList;
-};
-
-template <typename ELEMENT_TYPE>
-EXP std::ostream& operator << (std::ostream& os, const mfStack<ELEMENT_TYPE>& elt);
-
-template <typename ELEMENT_TYPE>
-EXP std::ostream& operator << (std::ostream& os, const SMARTP<mfStack<ELEMENT_TYPE>>& elt);
+// template <typename ELEMENT_TYPE>
+// class EXP mfStack : public smartable
+// /*
+//   C++ std::stack cannot be traversed, so we use std::list instead
+//
+//   https://www.programiz.com/cpp-programming/typename-templates
+//
+//   https://en.cppreference.com/w/cpp/language/type_alias
+//
+//   Do not put templated functions into the cpp file.
+//   Since they are not implementations they are not found by the compiler/linker.
+//   So leave them in the header file.
+//
+//   member reference base type 'int' is not a structure or union
+//
+//   https://stackoverflow.com/questions/580922/identifying-primitive-types-in-templates
+//
+//   https://en.cppreference.com/w/cpp/language/template_specialization
+//
+//   https://www.ibm.com/docs/en/zos/2.4.0?topic=only-explicit-specialization-c
+//
+//   https://dev.to/sandordargo/what-are-type-traits-in-c-18j5
+// */
+// {
+//   public:
+//
+//     // creation
+//     // ------------------------------------------------------
+//
+//     static SMARTP<mfStack> create ();
+//
+//     static SMARTP<mfStack> create (
+//                             const ELEMENT_TYPE& element);
+//
+//     // constructors/destructor
+//     // ------------------------------------------------------
+//
+//                           mfStack ();
+//
+//                           mfStack (const ELEMENT_TYPE& element);
+//
+//     virtual               ~mfStack ();
+//
+//   public:
+//
+//     // set and get
+//     // ------------------------------------------------------
+//
+//     const std::list <ELEMENT_TYPE>&
+//                           getElementsStdList () const
+//                               { return fElementsStdList;}
+//
+//   public:
+//
+//     // public services
+//     // ------------------------------------------------------
+//
+//     // basic list stuff
+//     size_t                size () const
+//                               { return fElementsStdList.size (); }
+//
+//     void                  push (
+//                             const ELEMENT_TYPE& element)
+//                               { fElementsStdList.push_front (element); }
+//
+//     ELEMENT_TYPE&         top ()
+//                               { return fElementsStdList.front (); }
+//
+//     void                  pop ()
+//                               { fElementsStdList.pop_front (); }
+//
+//     // specific stack stuff
+//     Bool                  contains (
+//                             const ELEMENT_TYPE& value) const;
+//
+//   public:
+//
+//     // print
+//     // ------------------------------------------------------
+//
+//     void                  print (std::ostream& os) const;
+//
+//     void                  printWithContext (
+//                             const std::string& context,
+//                             char               evidencer,
+//                             std::ostream&      os) const;
+//
+//   private:
+//
+//     // private services
+//     // ------------------------------------------------------
+//
+//   private:
+//
+//     // private fields
+//     // ------------------------------------------------------
+//
+//     std::string           fElementsStackName;
+//
+//     std::list <ELEMENT_TYPE>
+//                           fElementsStdList;
+// };
+//
+// template <typename ELEMENT_TYPE>
+// EXP std::ostream& operator << (std::ostream& os, const mfStack<ELEMENT_TYPE>& elt);
+//
+// template <typename ELEMENT_TYPE>
+// EXP std::ostream& operator << (std::ostream& os, const SMARTP<mfStack<ELEMENT_TYPE>>& elt);
 
 
 }
